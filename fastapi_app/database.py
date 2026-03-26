502ddb04ff4b8e5fc32e9dccf8cf846fef9e557e
20185326 <a20185326@pucp.edu.pe>
2026-03-26 09:05:08 -0500

    fix: convertir mysql:// a mysql+pymysql:// para Railway

--- fastapi_app/database.py
+++ fastapi_app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.environ.get("MYSQL_URL") or os.environ.get("DATABASE_URL") or "mysql://root:MpkNnooeopanrfteJGMETSfyNUBslzFt@interchange.proxy.rlwy.net:59309/railway"
+DATABASE_URL = DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
